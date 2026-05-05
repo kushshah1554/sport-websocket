9ab6e1d7f04c3b7d23d98f20cede659de8604766
kush shah <skush1554@gmail.com>
2026-05-05 23:41:41 +0545

    implement database schema

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+console.log("DB URL:", process.env.DATABASE_URL); 
 
 if(!process.env.DATABASE_URL) {
   throw new Error("Database_URL is not defined in .env file");
@@ -11,6 +12,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 });
