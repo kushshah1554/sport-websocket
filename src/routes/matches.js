ac1433677ada76cd796920229e71f1ea49ac5fdb
kush shah <skush1554@gmail.com>
2026-05-06 19:09:21 +0545

    fixed code

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -18,7 +18,7 @@ matchRouter.get("/", async (req, res) => {
   if (!parsed.success) {
     return res.status(400).json({
       error: "Invalid query parameters",
-      details: JSON.stringify(parsed.error),
+      details: parsed.error.issues,
     });
   }
 
@@ -43,14 +43,15 @@ matchRouter.get("/", async (req, res) => {
 
 matchRouter.post("/", async (req, res) => {
   const parsed = createMatchSchema.safeParse(req.body);
-  const { startTime, endTime, homeScore, awayScore } = parsed.data || {};
 
   if (!parsed.success) {
     return res.status(400).json({
       error: "invalid payload",
-      details: JSON.stringify(parsed.error),
+      details: parsed.error.issues,
     });
   }
+  const { data: { startTime, endTime, homeScore, awayScore } } = parsed;
+
 
   try {
     const [event] = await db
